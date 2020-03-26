84cb3c92aecc3c1082e2d8f006830b949c2f5e7c
analogy47 <actweed47@gmail.com>
2020-03-25 22:23:56 -0400

    sort descending

--- sort-tables/src/PlayerTable.js
+++ sort-tables/src/PlayerTable.js
@@ -3,21 +3,29 @@ import React from 'react';
 const PlayerTable = props => {
   const { players } = props;
 
-  const [sortedColumn, setSortedColumn] = React.useState(null); // sort by different columns
+  const [sortedColumn, setsortedColumn] = React.useState({}); // sort by different columns
 
   let sortedPlayers = [...players]; // copy of props to display sorted players
 
   if (sortedColumn !== null) {
     sortedPlayers.sort((a, b) => {
-      if (a[sortedColumn] < b[sortedColumn]) {
-        return -1;
+      if (a[sortedColumn.key] < b[sortedColumn.key]) {
+        return sortedColumn.direction === 'ascending' ? -1 : 1;
       }
-      if (a[sortedColumn] > b[sortedColumn]) {
-        return 1;
+      if (a[sortedColumn.key] > b[sortedColumn.key]) {
+        return sortedColumn.direction === 'ascending' ? 1 : -1;
       }
       return 0;
     });
   }
+
+  const getSortDirection = key => {
+    let direction = 'ascending';
+    if (sortedColumn.key === key && sortedColumn.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setsortedColumn({ key, direction });
+  };
 
   return (
     <table>
@@ -25,17 +33,17 @@ const PlayerTable = props => {
       <thead>
         <tr>
           <th>
-            <button type="button" onClick={() => setSortedColumn('name')}>
+            <button type="button" onClick={() => getSortDirection('name')}>
               Name
             </button>
           </th>
           <th>
-            <button type="button" onClick={() => setSortedColumn('position')}>
+            <button type="button" onClick={() => getSortDirection('position')}>
               Position
             </button>
           </th>
           <th>
-            <button type="button" onClick={() => setSortedColumn('number')}>
+            <button type="button" onClick={() => getSortDirection('number')}>
               Number
             </button>
           </th>
